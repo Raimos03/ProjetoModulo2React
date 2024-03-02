27c2aaadf944fa677d69764b00358a1ed2eb4860
Pedro Lima <pedrolimae3@gmail.com>
2024-03-01 21:49:16 -0300

    1.0.0

---
+++ PedroHenriqueLimaSilva/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return (
+        <>
+            <header>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Filmes</li>
+                    <li>Series</li>
+                </ul>
+                <h2> Eu sou a header</h2>
+            </header>
+        </>
+    );
+}
+
+export default Header;
